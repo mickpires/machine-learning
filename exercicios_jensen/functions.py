45f5e5089eb5dd5e6a71ddd8b42014b71116da00
mickpires <83048320+mickpires@users.noreply.github.com>
2024-05-31 12:51:42 -0300

    fiz alguns exerci

--- exercicios_jensen/functions.py
+++ exercicios_jensen/functions.py
@@ -2,7 +2,7 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
-from numpy import sqrt
+import numpy as np
 
 def getRmse(X_train,y_train,X_test,y_test,degree):
     model = Pipeline([
@@ -11,6 +11,7 @@ def getRmse(X_train,y_train,X_test,y_test,degree):
         ('lin_reg', LinearRegression())
     ])
     model.fit(X_train,y_train)
-    rmse_train = (sqrt(mean_squared_error(y_train,model.predict(X_train))))
-    rmse_test = sqrt(mean_squared_error(y_test,model.predict(X_test)))
+    rmse_train = (np.sqrt(mean_squared_error(y_train,model.predict(X_train))))
+    rmse_test = np.sqrt(mean_squared_error(y_test,model.predict(X_test)))
     return rmse_train, rmse_test
+
