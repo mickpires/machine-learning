665af40e2a79623198b29dcce5aebf9d4a423804
mickpires <83048320+mickpires@users.noreply.github.com>
2024-05-31 20:48:47 -0300

    cross validation

--- exercicios_jensen/functions.py
+++ exercicios_jensen/functions.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 import numpy as np
+from math import floor
 
 def getRmse(X_train,y_train,X_test,y_test,degree):
     model = Pipeline([
@@ -22,3 +23,18 @@ def eqmByComplexity(X_train,y_train,X_test,y_test,degree):
         ('lin_reg', LinearRegression())
     ])
     model.fit(X_train,y_train)
+
+
+def KFold(x,y,k):
+    k_fold_size = floor(len(y)/k)
+    y_new = np.empty((k,k_fold_size))
+    x_new = np.empty((k,k_fold_size))
+    for j in range(k_fold_size):
+        for i in range(k):
+            index = np.random.randint(0,len(y))
+            y_new[i,j] = y[index]
+            x_new[i,j] = x[index]
+            if len(y) > 0:
+                y = np.delete(y,index)
+                x = np.delete(x,index)
+    return x_new,y_new
